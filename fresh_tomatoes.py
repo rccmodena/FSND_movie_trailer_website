9946487aabbbafaf16cf0221d1bbc42fbd7fb00e
Rudi César Comiotto Modena <rudi.modena@gmail.com>
2019-02-02 16:08:02 -0200

    Updated fresh_tomatoes.py

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -4,149 +4,170 @@ import re
 
 
 # Styles and scripting for the page
-main_page_head = '''
-<!DOCTYPE html>
+main_page_head = '''<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Fresh Tomatoes!</title>
+    <title>Project: Movie Trailer Website!</title>
 
-    <!-- Google Font Baloo Thambi -->
-    <link href="https://fonts.googleapis.com/css?family=Baloo+Thambi" rel="stylesheet">
+    <!-- Bootstrap 4 -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 
-    <!-- Bootstrap 3 -->
-    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
-    <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
-    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
-    <style type="text/css" media="screen">
-        body {
-            padding-top: 80px;
-            background-color: #03211C;
-            color: #7d9267;
-            font-family: 'Baloo Thambi', cursive;
+    <style media="screen">
+        main {
+            padding-top: 30px;
         }
-        #trailer .modal-dialog {
-            margin-top: 200px;
-            width: 640px;
-            height: 480px;
+        .movie-card {
+            padding: 20px;
         }
-        .hanging-close {
-            position: absolute;
-            top: -12px;
-            right: -12px;
-            z-index: 9001;
-        }
-        #trailer-video {
-            width: 100%;
-            height: 100%;
-        }
-        .movie-tile {
-            margin-bottom: 20px;
-            padding-top: 20px;
-            border-radius: 40px;
-        }
-        .movie-tile:hover {
-            background-color: #03342e;
+        .movie-card:hover {
+            background-color: #b1bfa6;
             cursor: pointer;
         }
-        .scale-media {
-            padding-bottom: 56.25%;
-            position: relative;
+        .modal-title{
+            font-weight: bold;
         }
-        .scale-media iframe {
-            border: none;
-            height: 100%;
-            position: absolute;
-            width: 100%;
-            left: 0;
-            top: 0;
-            background-color: white;
-        }
-        #id_navbar_top {
-            background: #7d9267;
-        }
-        #id_navbar-brand {
-            color: #03211C;
-        }
-        #id_navbar-brand:hover {
-            color: yellow;
-        }
-
     </style>
-    <script type="text/javascript" charset="utf-8">
-        // Pause the video when the modal is closed
-        $(document).on('click', '.hanging-close, .modal-backdrop, .modal', function (event) {
-            // Remove the src so the player itself gets removed, as this is the only
-            // reliable way to ensure the video stops playing in IE
-            $("#trailer-video-container").empty();
+
+    <script>
+        // Open the modal with movie information when clicking on the movie cards
+        $(document).on('click', '.movie-card', function (event) {
+
+            // Gather the information about the movie
+            var movie_title = $(this).attr('data-movie-title')
+            var movie_year = $(this).attr('data-movie-year')
+            var movie_wikipedia = $(this).attr('data-wikipedia')
+            var movie_rotten_tomatoes = $(this).attr('data-rotten-tomatoes')
+            var movie_imdb = $(this).attr('data-imdb')
+
+            //Open the modal with the movie details
+            $('#ModalMoviedetails').modal('toggle')
+
+            //Display the correct movie title and year
+            $('#ModalMoviedetailsTitle').text(movie_title + ' ('+movie_year+')')
+
+            //Add the correct path to wikipedia page of the movie
+            //If the wikipedia page was not informed, set the attribute onClick empty
+            if (movie_wikipedia != "#"){
+                $('#id_wikipedia').attr('onClick', "window.open('"+movie_wikipedia+"', '_blank');")
+            } else {
+                $('#id_wikipedia').attr('onClick', '');
+            }
+
+            //Add the correct path to rotten tomatoes page of the movie
+            //If the rotten tomatoes page was not informed, set the attribute onClick empty
+            if (movie_rotten_tomatoes != "#"){
+                $('#id_rotten_tomatoes').attr('onClick', "window.open('"+movie_rotten_tomatoes+"', '_blank');")
+            } else {
+                $('#id_rotten_tomatoes').attr('onClick', '');
+            }
+
+            //Add the correct path to imbdb page of the movie
+            //If the imdb page was not informed, set the attribute onClick empty
+            if (movie_imdb != "#"){
+                $('#id_imdb').attr('onClick', "window.open('"+movie_imdb+"', '_blank');")
+            } else {
+                $('#id_imdb').attr('onClick', '');
+            }
         });
-        // Start playing the video whenever the trailer modal is opened
-        $(document).on('click', '.movie-tile', function (event) {
-            var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
-            var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
-            $("#trailer-video-container").empty().append($("<iframe></iframe>", {
-              'id': 'trailer-video',
-              'type': 'text-html',
-              'src': sourceUrl,
-              'frameborder': 0
-            }));
-        });
+
         // Animate in the movies when the page loads
         $(document).ready(function () {
-          $('.movie-tile').hide().first().show("fast", function showNext() {
-            $(this).next("div").show("fast", showNext);
-          });
+            $('.movie-card').hide().first().show("fast", function showNext() {
+                $(this).next("div").show("fast", showNext);
+            });
         });
+
     </script>
+
+
 </head>
 '''
 
 
 # The main page layout and title bar
 main_page_content = '''
-  <body>
-    <!-- Trailer Video Modal -->
-    <div class="modal" id="trailer">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <a href="#" class="hanging-close" data-dismiss="modal" aria-hidden="true">
-            <img src="https://lh5.ggpht.com/v4-628SilF0HtHuHdu5EzxD7WRqOrrTIDi_MhEG6_qkNtUK5Wg7KPkofp_VJoF7RS2LhxwEFCO1ICHZlc-o_=s0#w=24&h=24"/>
-          </a>
-          <div class="scale-media" id="trailer-video-container">
-          </div>
-        </div>
-      </div>
-    </div>
+    <body>
 
-    <!-- Main Page Content -->
-    <div class="container">
-      <div class="navbar navbar-fixed-top" role="navigation" id="id_navbar_top">
-        <div class="container">
-          <div class="navbar-header">
-            <a id="id_navbar-brand" class="navbar-brand" href="#">Fresh Tomatoes Movie Trailers</a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="container">
-      {movie_tiles}
-    </div>
-  </body>
+        <header>
+
+            <!-- Navbar -->
+            <div class="pos-f-t">
+                <nav class="navbar navbar-dark bg-dark">
+                    <a class="navbar-brand" href="#">My Favourite Movies</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNanodegreeDetails" aria-controls="navbarNanodegreeDetails" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </nav>
+                <div class="collapse" id="navbarNanodegreeDetails">
+                    <div class="navbar-nav bg-dark p-4">
+                        <h5 class="text-white h5">Project: Movie Trailer Website!</h5>
+                        <div class="text-muted">
+                            <p>This is the project Movie Trailer Website, part of the Full Stack Web Developer Nanodegree (Udacity).</p>
+                            <p><a class="badge badge-info" href="https://github.com/rccmodena/movie_trailer_website" target="_blank">Click here</a> to go to the repository's page.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </header>
+
+        <main>
+
+            <!-- Modal with the Movie Information -->
+            <div class="modal fade" id="ModalMoviedetails" tabindex="-1" role="dialog" aria-labelledby="ModalMoviedetailsTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="ModalMoviedetailsTitle">Modal title</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <button type="button" class="btn btn-danger btn-lg btn-block"><strong>Watch the Trailer</strong></button>
+                            <button id="id_wikipedia" type="button" class="btn btn-info btn-lg btn-block"><strong>Wikipedia</strong></button>
+                            <button id="id_rotten_tomatoes" type="button" class="btn btn-info btn-lg btn-block"><strong>Rotten Tomatoes</strong></button>
+                            <button id="id_imdb" type="button" class="btn btn-info btn-lg btn-block"><strong>IMDb</strong></button>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Container with the Movie Cards -->
+            <div class="container">
+                <div class="row">
+                    {movie_cards}
+                </div>
+            </div>
+        </main>
+    </body>
 </html>
 '''
 
 
+
 # A single movie entry html template
-movie_tile_content = '''
-<div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
-    <img src="{poster_image_url}" width="220" height="342">
-    <h2>{movie_title}</h2>
+movie_cards_content = '''
+<div class="col-md-6 col-lg-4 movie-card" data-trailer-youtube-id="{trailer_youtube_id}" data-movie-title="{movie_title}" data-movie-year="{movie_year}" data-wikipedia="{movie_wikipedia}" data-rotten-tomatoes="{movie_rotten_tomatoes}" data-imdb="{movie_imdb}">
+    <div class="card text-white bg-dark h-100">
+        <div class="card-body text-center">
+            <img src="{poster_image_url}" alt="{poster_alt}" width="220" height="342">
+            <h5>{movie_title}</h5>
+            <h5>({movie_year})</h5>
+        </div>
+    </div>
 </div>
 '''
 
 
-def create_movie_tiles_content(movies):
+def create_movie_cards_content(movies):
     # The HTML content for this section of the page
     content = ''
     for movie in movies:
@@ -159,9 +180,14 @@ def create_movie_tiles_content(movies):
                               else None)
 
         # Append the tile for the movie with its content filled in
-        content += movie_tile_content.format(
+        content += movie_cards_content.format(
             movie_title=movie.get_title(),
+            movie_year=movie.get_year(),
             poster_image_url=movie.get_poster_image(),
+            poster_alt=movie.get_title()+" Movie Poster.",
+            movie_wikipedia=movie.get_wikipedia(),
+            movie_rotten_tomatoes=movie.get_rotten_tomatoes(),
+            movie_imdb=movie.get_imdb(),
             trailer_youtube_id=trailer_youtube_id
         )
     return content
@@ -173,7 +199,7 @@ def open_movies_page(movies):
 
     # Replace the movie tiles placeholder generated content
     rendered_content = main_page_content.format(
-        movie_tiles=create_movie_tiles_content(movies))
+        movie_cards=create_movie_cards_content(movies))
 
     # Output the file
     output_file.write(main_page_head + rendered_content)
